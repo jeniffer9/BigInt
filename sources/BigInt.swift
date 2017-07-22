15483b35e30fd827f82a2b3fbfc57bc84eeb93b5
alexander-san <alexander.sanchez@epfl.ch>
2017-07-23 00:07:16 +0200

    trying UnsignedRational

--- sources/BigInt.swift
+++ sources/BigInt.swift
@@ -29,9 +29,9 @@ import SipHash
 /// ```
 ///
 public struct BigInt {
-    public enum Sign {
-        case plus
-        case minus
+    public enum Sign: Int {
+        case plus = 1
+        case minus = -1
     }
 
     public typealias Magnitude = BigUInt
